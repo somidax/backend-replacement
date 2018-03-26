560d69e00e08e96cc783dc13f1b669ba25725a91
SOMIDAX <23489369+somidax@users.noreply.github.com>
2018-03-26 01:20:56 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ HTTP_PROVIDER_URL = environ.get("HTTP_PROVIDER_URL")
 WS_PROVIDER_URL = environ.get("WS_PROVIDER_URL")
 
 ED_CONTRACT_ADDR = '0x8d12a197cb00d4747a1fe03395095ce2a5cc6819'
-with open('etherdelta.abi.json') as f:
+with open('coinEstate.abi.json') as f:
     import json
     ED_CONTRACT_ABI = json.load(f)
 ED_WS_SERVERS = [
@@ -21,4 +21,4 @@ POSTGRES_DB = environ.get("POSTGRES_DB")
 POSTGRES_USER = environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD")
 
-FRONTEND_CONFIG_FILE="https://raw.githubusercontent.com/forkdelta/forkdelta.github.io/master/config/main.json"
+FRONTEND_CONFIG_FILE="https://raw.githubusercontent.com/somidax/coinEstate/master/config/main.json"
