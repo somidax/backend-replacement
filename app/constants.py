7a77d6fb9f35671841b21cd5f66c42556b08516a
SOMIDAX <23489369+somidax@users.noreply.github.com>
2018-04-15 16:11:45 +0100

    update access point  to server

---
+++ app/constants.py
@@ -0,0 +1,10 @@
+# Eternally constant constants
+
+ZERO_ADDR = "0x0000000000000000000000000000000000000000"
+ZERO_ADDR_BYTES = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+
+# access endpoint
+
+FILTER_ORDERS_UNDER_ETH = 0.001
+ALLOWED_ORIGIN_SUFFIXES = ('https://coinestate.somidax.net','https://api.somidax.net','deltabalances.github.io','https://github.com/somidax/coinEstate', 'localhost', 'devd.io')
