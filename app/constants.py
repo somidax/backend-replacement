eb17b86b0b9e346ca9ef9b42fe87e2c9237c639b
SOMIDAX <23489369+somidax@users.noreply.github.com>
2018-04-15 16:24:29 +0100

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -7,4 +7,4 @@ ZERO_ADDR_BYTES = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00
 # access endpoint
 
 FILTER_ORDERS_UNDER_ETH = 0.001
-ALLOWED_ORIGIN_SUFFIXES = ('https://coinestate.somidax.net','https://api.somidax.net','deltabalances.github.io','https://github.com/somidax/coinEstate', 'localhost', 'devd.io')
+ALLOWED_ORIGIN_SUFFIXES = ('"https://somidax.net/",https://coinestate.somidax.net','https://api.somidax.net','deltabalances.github.io','https://github.com/somidax/coinEstate', 'localhost', 'devd.io')
